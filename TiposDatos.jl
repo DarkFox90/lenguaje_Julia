a9587b9b273736d7957e2ec01e1119007f991b9d
DarkFox90 <carlospozuelos3@gmail.com>
2026-04-14 18:13:07 -0600

    Fase 2 de investigacion de lenguaje

---
+++ TiposDatos.jl
@@ -0,0 +1,13 @@
+lista_idiomas = ["Espanol", "ingles", "frances", "italiano", "portugues"]
+
+libros = Dict("Harry Potter" => "J.K. Rowling", "Metro 2033" => "Dmitry Glukhovsky", "El señor presidente" => "Miguel Angel Asturias")
+
+nombres = Set(["Carlos", "Anika", "Katia", "Mia", "Javier", "Fabricio", "Emilia"])
+
+abecedario = ('a', 'b', 'c', 'd', 'e')
+
+println("idiomas: ", lista_idiomas)
+println("libros: ", libros)
+println("nombres: ", nombres)
+println("abecedario: ", abecedario)
+println("El primer idioma en la lista es: ", lista_idiomas[1])
